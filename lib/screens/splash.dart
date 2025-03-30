bef1939856dd4268b27dcf4a52498e5c0f21bd85
Zilvinas <zilvinas.skuodys@gmail.com>
2025-03-30 14:58:27 +0800

    test

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -10,7 +10,7 @@ class SplashScreen extends StatelessWidget {
         title: const Text('Chat'),
       ),
       body: const Center(
-        child: Text('Loading'),
+        child: Text('Loading...'),
       ),
     );
   }
